12749626c4e92d473ebf15b93864c83877affbad
= <=>
2023-08-27 02:27:54 +0500

    Adding forms with api calls

---
+++ ui/src/components/Forms/LoginForm.tsx
@@ -0,0 +1,78 @@
+import { Josefin_Sans } from "next/font/google";
+import Link from "next/link";
+
+const josef = Josefin_Sans({ subsets: ["latin"] });
+const LoginForm = () => {
+  const submitForm = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formElements = form.elements as typeof form.elements & {
+      email: HTMLInputElement;
+      password: HTMLInputElement;
+    };
+    const data = {
+      email: formElements.email.value,
+      password: formElements.password.value,
+    };
+    await fetch("http://localhost:5000/api/user/login", {
+      method: "POST",
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({
+        ...data,
+      }),
+    })
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        alert(JSON.stringify(data));
+      });
+  };
+  return (
+    <div
+      className={`flex place-content-center ${josef.className} drop-shadow-md`}
+    >
+      <form
+        className="flex flex-col items-center bg-prod mx-4 rounded justify-center h-80 w-80 m-24"
+        onSubmit={submitForm}
+      >
+        <div className="py-3 text-xl">Login Form</div>
+        <input
+          title="Email"
+          placeholder="Please Enter Email"
+          type="text"
+          id="email"
+          className="bg-white shadow-sm focus:outline-input outline outline-input w-60 rounded-l p-2"
+        />
+        <input
+          title="Password"
+          placeholder="Please Enter Password"
+          type="password"
+          id="password"
+          className="bg-white shadow-sm focus:outline-input outline outline-input w-60 rounded-l p-2 m-2"
+        />
+        <input
+          type="submit"
+          title="Login"
+          className=" rounded text-white bg-pink p-2 w-60 mt-3"
+        />
+        <div className="mt-2">Don't have an account?</div>
+        <Link
+          href={"/SignUp"}
+          className="rounded text-center text-white bg-pink p-2 w-40"
+        >
+          Sign Up here
+        </Link>
+      </form>
+    </div>
+  );
+};
+export default LoginForm;
---
+++ ui/src/pages/Login.tsx
@@ -0,0 +1,33 @@
+import { GetStaticProps } from "next";
+import Layout from "@/components/Layout";
+import { Josefin_Sans } from "next/font/google";
+import NotFoundPicture from "../Assets/NotFound.jpg";
+import LoginForm from "@/components/Forms/LoginForm";
+const josef = Josefin_Sans({ subsets: ["latin"] });
+
+type Props = {
+  NotFoundImage: any;
+  errors?: string;
+};
+
+export default function Login({ NotFoundImage }: Props) {
+  return (
+    <div className={josef.className}>
+      <Layout>
+        <LoginForm />
+      </Layout>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    // By returning { props: item }, the StaticPropsDetail component
+    // will receive `item` as a prop at build time
+    const NotFoundImage = "NotFoundPicture";
+
+    return { props: { NotFoundImage } };
+  } catch (err: any) {
+    return { props: { errors: err.message } };
+  }
+};
