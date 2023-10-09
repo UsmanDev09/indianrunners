f7924817047afac535f31becada45ff3ed5bcccd
= <=>
2023-10-09 17:34:25 +0500

    bug fixed

--- server/models/badge.ts
+++ server/models/badge.ts
@@ -1,4 +1,4 @@
-import { InferSchemaType, model, Schema } from 'mongoose'
+import { InferSchemaType, model, Schema, Types } from 'mongoose'
 
 const badgeSchema = new Schema({
     name: { type: String, required: [true, 'Badge name is required'] },
@@ -14,7 +14,13 @@ const badgeSchema = new Schema({
     }
 }, { timestamps: true })
 
-export type Badge = InferSchemaType<typeof badgeSchema>
 
+export interface Badge extends Document {
+    _id: Types.ObjectId
+    name: string
+    description: string
+    image: string
+    criteria: object
+}
 
 export default model<Badge>('Badge', badgeSchema)
