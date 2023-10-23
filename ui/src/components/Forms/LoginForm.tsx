d94bdb628102155b8863a9c27c3b3bd7a2b7fc91
= <=>
2023-10-23 10:32:26 +0500

    Color changes confined with theme

--- ui/src/components/Forms/LoginForm.tsx
+++ ui/src/components/Forms/LoginForm.tsx
@@ -73,9 +73,9 @@ const LoginForm = () => {
         className="flex flex-col items-center bg-prod dark:bg-dark rounded justify-center h-screen w-screen"
         onSubmit={submitForm}
       >
-        <section className="bg-gray-50 dark:bg-gray-900 sm:w-1/2">
+        <section className="bg-gray-100 dark:bg-dark-green sm:w-1/2">
           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0 dark:bg-prodblue dark:border-gray-700">
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-700 dark:border-gray-700">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Log In
@@ -103,6 +103,7 @@ const LoginForm = () => {
                     Password
                   </label>
                   <input
+                    type='password'
                     onChange={(e) =>
                       setFormData({ ...formData, password: e.target.value })
                     }
@@ -110,39 +111,15 @@ const LoginForm = () => {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
                 </div>
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="terms"
-                      aria-describedby="terms"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label
-                      htmlFor="terms"
-                      className="font-light text-gray-500 dark:text-white"
-                    >
-                      I accept the{" "}
-                      <a
-                        className="font-medium text-primary-600 hover:underline dark:text-white"
-                        href="#"
-                      >
-                        Terms and Conditions
-                      </a>
-                    </label>
-                  </div>
-                </div>
                 <button
                   type="submit"
-                  className="w-full text-white bg-gray hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="w-full text-white bg-gray-700 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
                 >
                   Log In
                 </button>
                 <Link
                   href={"/signup"}
-                  className="rounded text-center text-black text-underline underline  p-2 dark: text-white"
+                  className="rounded text-center text-black text-underline underline  p-2 dark:text-white"
                 >
                   Sign Up
                 </Link>
