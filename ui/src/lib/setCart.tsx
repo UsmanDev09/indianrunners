900f3d910c7d21ca8136335cea5f1c6ce84a8d7a
= <=>
2024-01-16 20:33:20 +0500

    deleted unused code

--- ui/src/lib/setCart.tsx
+++
@@ -1,9 +0,0 @@
-const setCart = (
-  Count: number | string | null,
-  Bill: number | string | null
-): void => {
-  localStorage.setItem("Count", Count as string);
-  localStorage.setItem("Bill", Bill as string);
-};
-
-export default setCart;
